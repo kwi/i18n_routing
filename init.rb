b6e17c9ce52dc83efab347bbc7dc517ae50ae831
Kwi <guillaume.luccisano@gmail.com>
2010-03-16 17:18:08 +0100

    Fix booting and README

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 if Rails.version < '3'
-  require File.join(File.dirname(__FILE__), 'lib', 'i18n_routing_rails2', '*.rb')
+  require File.join(File.dirname(__FILE__), 'lib', 'i18n_routing_rails2')
 else
-  require File.join(File.dirname(__FILE__), 'lib', 'i18n_routing_rails3', '*.rb')
+  require File.join(File.dirname(__FILE__), 'lib', 'i18n_routing_rails3')
 end
