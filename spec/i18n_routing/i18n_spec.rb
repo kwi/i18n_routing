5e0f9cf67eb4d24947bcc148e8c4e62de019e827
Big Tiger <bigtiger@hashrocket.com>
2010-04-26 21:48:28 +0800

    Rewrite i18n spec file for clarity
    - Use let
    - Add context
    - Better describe tests
    - Fix Grammar

--- spec/i18n_routing/i18n_spec.rb
+++ spec/i18n_routing/i18n_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe :localized_routes do
   before(:all) do
 
-    puts "Route for localize spec"
     ActionController::Routing::Routes.clear!
 
     if Rails.version < '3'
@@ -59,136 +58,154 @@ describe :localized_routes do
 
     end
 
-
     class UrlTester
       include ActionController::UrlWriter
     end
 
-    @routes = UrlTester.new
   end
 
-  def routes
-    @routes
-  end
+  let(:nested_routes) { ActionController::Routing::Routes.named_routes.instance_eval { routes } }
+  let(:routes) { UrlTester.new }
 
   def url_for(opts)
     ActionController::Routing::Routes.generate_extras(opts).first
   end
 
-  it "should still work for non localized named_route" do
-    routes.send(:not_about_path).should == "/not_about"
-  end
+  context "do not break existing behavior" do
 
-  it "should still work for non localized resource" do
-    routes.send(:not_contact_path).should == "/not_contact"
-  end
-
-  it "should still work for non localized resources" do
-    routes.send(:not_users_path).should == "/not_users"
-  end
-
-  it "should works for localized named_route" do
-    I18n.locale = :fr
-    routes.send(:about_path).should == "/#{I18n.t :about, :scope => :named_routes_path}"
-    I18n.locale = :en
-    routes.send(:about_path).should == "/#{I18n.t :about, :scope => :named_routes_path}"
-    I18n.locale = :de # Not localized
-    routes.send(:about_path).should == "/about"
-  end
-
-  it "should works for localized resource" do
-    I18n.locale = :fr
-    routes.send(:contact_path).should == "/#{I18n.t :contact, :scope => :resource}"
-    I18n.locale = :en
-    routes.send(:contact_path).should == "/#{I18n.t :contact, :scope => :resource}"
-    I18n.locale = :de # Not localized
-    routes.send(:contact_path).should == "/contact"
-  end
-
-  it "should works for localized resources" do
-    I18n.locale = :fr
-    routes.send(:users_path).should == "/#{I18n.t :users, :scope => :resources}"
-    I18n.locale = :en
-    routes.send(:users_path).should == "/#{I18n.t :users, :scope => :resources}"
-    I18n.locale = :de # Not localized
-    routes.send(:users_path).should == "/users"
-  end
-
-  it "should works with url_for" do
-    I18n.locale = :fr
-    url_for(:controller => :users).should == "/#{I18n.t :users, :scope => :resources}"
-    url_for(:controller => :about, :action => :show).should == "/#{I18n.t :about, :scope => :named_routes_path}"
-    I18n.locale = :en
-    url_for(:controller => :users).should == "/#{I18n.t :users, :scope => :resources}"
-    I18n.locale = :de # Not localized
-    url_for(:controller => :users).should == "/users"
-  end
-
-  it "should have correct controller requirements" do
-
-  end
-
-  it "should main resources works with nested resources" do
-    I18n.locale = :fr
-    routes.send(:authors_path).should == "/#{I18n.t :authors, :scope => :resources}"
-    I18n.locale = :de # Not localized
-    routes.send(:authors_path).should == "/authors"
-  end
-
-  it "should works with nested resources" do
-    I18n.locale = :fr
-    routes.send(:author_books_path, 1).should == "/#{I18n.t :authors, :scope => :resources}/1/#{I18n.t :books, :scope => :resources}"
-    I18n.locale = :de # Not localized
-    routes.send(:author_books_path, 1).should == "/authors/1/books"
-  end
-
-  it "should works with deep nested resources" do
-    I18n.locale = :fr
-    routes.send(:universes_path).should == "/#{I18n.t :universes, :scope => :resources}"
-    routes.send(:universe_galaxies_path, 1).should == "/#{I18n.t :universes, :scope => :resources}/1/#{I18n.t :galaxies, :scope => :resources}"
-    routes.send(:universe_galaxy_planets_path, 1, 1).should == "/#{I18n.t :universes, :scope => :resources}/1/#{I18n.t :galaxies, :scope => :resources}/1/#{I18n.t :planets, :scope => :resources}"
-    I18n.locale = :de # Not localized
-    routes.send(:universes_path).should == "/universes"
-    routes.send(:universe_galaxies_path, 1).should == "/universes/1/galaxies"
-    routes.send(:universe_galaxy_planets_path, 1, 1).should == "/universes/1/galaxies/1/planets"
-  end
-
-  it "should nested resources have correct significant_keys" do
-    r = ActionController::Routing::Routes.named_routes.instance_eval { @routes }
-
-    r[:author_fr_books].should_not be_nil
-
-    if Rails.version < '3'
-      r[:author_books].significant_keys.should include(:author_id)
-      r[:author_fr_books].significant_keys.should include(:author_id)
-    else
-      r[:author_books].segment_keys.should include(:author_id)
-      r[:author_fr_books].segment_keys.should include(:author_id)
+    it "of named_routes" do
+      routes.send(:not_about_path).should == "/not_about"
     end
-  end
 
-  it "should deep nested resources have correct significant_keys" do
-    r = ActionController::Routing::Routes.named_routes.instance_eval { @routes }
-
-    r[:universe_galaxy_fr_planet].should_not be_nil
-
-    if Rails.version < '3'
-      r[:universe_galaxy_planet].significant_keys.should include(:universe_id)
-      r[:universe_galaxy_fr_planet].significant_keys.should include(:universe_id)
-      r[:universe_galaxy_planet].significant_keys.should include(:galaxy_id)
-      r[:universe_galaxy_fr_planet].significant_keys.should include(:galaxy_id)
-    else
-      r[:universe_galaxy_planet].segment_keys.should include(:universe_id)
-      r[:universe_galaxy_fr_planet].segment_keys.should include(:universe_id)
-      r[:universe_galaxy_planet].segment_keys.should include(:galaxy_id)
-      r[:universe_galaxy_fr_planet].segment_keys.should include(:galaxy_id)
+    it "of a singular resource" do
+      routes.send(:not_contact_path).should == "/not_contact"
     end
+
+    it "of resources" do
+      routes.send(:not_users_path).should == "/not_users"
+    end
+
   end
 
-  it "should nested resources not deep translate with multi helpers" do
-    r = ActionController::Routing::Routes.named_routes.instance_eval { @routes }
+  context "for default routes" do
 
-    r.keys.should_not include(:fr_author_books) # Whant fr_author_books
+    before do
+      I18n.locale = :de # Not localized
+    end
+
+    it "named_route uses default values" do
+      routes.send(:about_path).should == "/about"
+    end
+
+    it "resource generates routes using default values" do
+      routes.send(:contact_path).should == "/contact"
+    end
+
+    it "resources generates routes using default values" do
+      routes.send(:users_path).should == "/users"
+    end
+
+    it "url_for generates route using default values" do
+      url_for(:controller => :users).should == "/users"
+    end
+
+    it "nested resources generate routes using default values" do
+      routes.send(:author_books_path, 1).should == "/authors/1/books"
+    end
+
+    it "deep nested resources generate routes using default values" do
+      routes.send(:universes_path).should == "/universes"
+      routes.send(:universe_galaxies_path, 1).should == "/universes/1/galaxies"
+      routes.send(:universe_galaxy_planets_path, 1, 1).should == "/universes/1/galaxies/1/planets"
+    end
+
+  end
+
+  context "" do
+
+    before do
+      I18n.locale = :fr
+    end
+
+    it "named_route generates route using localized values" do
+      routes.send(:about_path).should == "/#{I18n.t :about, :scope => :named_routes_path}"
+    end
+
+    it "resource generates routes using localized valuess" do
+      routes.send(:contact_path).should == "/#{I18n.t :contact, :scope => :resource}"
+    end
+
+    it "resources generates routes using localized values" do
+      routes.send(:users_path).should == "/#{I18n.t :users, :scope => :resources}"
+    end
+
+    it "url_for generates routes using localized values" do
+      url_for(:controller => :users).should == "/#{I18n.t :users, :scope => :resources}"
+      url_for(:controller => :about, :action => :show).should == "/#{I18n.t :about, :scope => :named_routes_path}"
+    end
+
+    it "nested resources generate routes using localized values" do
+      routes.send(:author_books_path, 1).should == "/#{I18n.t :authors, :scope => :resources}/1/#{I18n.t :books, :scope => :resources}"
+    end
+
+    it "deep nested resources generate routes using localized values" do
+      routes.send(:universes_path).should == "/#{I18n.t :universes, :scope => :resources}"
+      routes.send(:universe_galaxies_path, 1).should == "/#{I18n.t :universes, :scope => :resources}/1/#{I18n.t :galaxies, :scope => :resources}"
+      routes.send(:universe_galaxy_planets_path, 1, 1).should == "/#{I18n.t :universes, :scope => :resources}/1/#{I18n.t :galaxies, :scope => :resources}/1/#{I18n.t :planets, :scope => :resources}"
+    end
+
+    context "when nested" do
+
+      it "named routes should not be nil" do
+        nested_routes[:author_fr_books].should_not be_nil
+      end
+
+      context "in Rails < 3" do
+        it "include the correct significant keys" do
+          nested_routes[:author_books].significant_keys.should include(:author_id)
+          nested_routes[:author_fr_books].significant_keys.should include(:author_id)
+        end
+      end
+
+      context "in Rails 3" do
+        it "include the correct segment keys" do
+          nested_routes[:author_books].segment_keys.should include(:author_id)
+          nested_routes[:author_fr_books].segment_keys.should include(:author_id)
+        end
+      end
+
+    end
+
+    context "when deeply nested" do
+
+      it "named routes should not be nil" do
+        nested_routes[:universe_galaxy_fr_planet].should_not be_nil
+      end
+
+      context "in Rails < 3" do
+        it "include the correct significant keys" do
+          nested_routes[:universe_galaxy_planet].significant_keys.should include(:universe_id)
+          nested_routes[:universe_galaxy_fr_planet].significant_keys.should include(:universe_id)
+          nested_routes[:universe_galaxy_planet].significant_keys.should include(:galaxy_id)
+          nested_routes[:universe_galaxy_fr_planet].significant_keys.should include(:galaxy_id)
+        end
+      end
+
+      context "in Rails 3" do
+        it "include the correct segment keys" do
+          nested_routes[:universe_galaxy_planet].segment_keys.should include(:universe_id)
+          nested_routes[:universe_galaxy_fr_planet].segment_keys.should include(:universe_id)
+          nested_routes[:universe_galaxy_planet].segment_keys.should include(:galaxy_id)
+          nested_routes[:universe_galaxy_fr_planet].segment_keys.should include(:galaxy_id)
+        end
+      end
+
+    end
+
+    it "nested resources do not deep translate with multi helpers" do
+      nested_routes.keys.should_not include(:fr_author_books) # Want fr_author_books
+    end
+
   end
 
 end
